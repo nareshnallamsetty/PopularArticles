1ce5234285c2f8f122cb1630704e3a830a959fca
Naresh Nallamsetty <naresh.n@admins-MacBook-Pro.local>
2019-04-02 15:51:39 +0530

    Seperated the model files

---
+++ PopularArticles/App-Modules/Articles-List-Module/entity/Results.swift
@@ -0,0 +1,63 @@
+//
+//  Results.swift
+//  PopularArticles
+//
+//  Created by Naresh Nallamsetty on 02/04/19.
+//  Copyright © 2019 Naresh Nallamsetty. All rights reserved.
+//
+
+import Foundation
+
+struct Results : Codable {
+    let url : String?
+    let adx_keywords : String?
+    let column : String?
+    let section : String?
+    let byline : String?
+    let type : String?
+    let title : String?
+    let abstract : String?
+    let published_date : String?
+    let source : String?
+    let id : Int?
+    let asset_id : Int?
+    let views : Int?
+    let media : [Media]?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case url = "url"
+        case adx_keywords = "adx_keywords"
+        case column = "column"
+        case section = "section"
+        case byline = "byline"
+        case type = "type"
+        case title = "title"
+        case abstract = "abstract"
+        case published_date = "published_date"
+        case source = "source"
+        case id = "id"
+        case asset_id = "asset_id"
+        case views = "views"
+        case media = "media"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        url = try values.decodeIfPresent(String.self, forKey: .url)
+        adx_keywords = try values.decodeIfPresent(String.self, forKey: .adx_keywords)
+        column = try values.decodeIfPresent(String.self, forKey: .column)
+        section = try values.decodeIfPresent(String.self, forKey: .section)
+        byline = try values.decodeIfPresent(String.self, forKey: .byline)
+        type = try values.decodeIfPresent(String.self, forKey: .type)
+        title = try values.decodeIfPresent(String.self, forKey: .title)
+        abstract = try values.decodeIfPresent(String.self, forKey: .abstract)
+        published_date = try values.decodeIfPresent(String.self, forKey: .published_date)
+        source = try values.decodeIfPresent(String.self, forKey: .source)
+        id = try values.decodeIfPresent(Int.self, forKey: .id)
+        asset_id = try values.decodeIfPresent(Int.self, forKey: .asset_id)
+        views = try values.decodeIfPresent(Int.self, forKey: .views)
+        media = try values.decodeIfPresent([Media].self, forKey: .media)
+    }
+    
+}
